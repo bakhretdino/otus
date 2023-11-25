b07b18b221c03e6dbed4063235f851e1e301b39d
Artem Bakhretdinov <bakhretdino@mail.ru>
2023-11-25 17:49:29 +0600

    Remove

--- libs/graphics_editor/primitives/primitives.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __PRIMITIVES__
-#define __PRIMITIVES__
-
-#include <circle.hpp>
-
-#endif /* __PRIMITIVES__ */
--- tests/libs/version/version_test.cpp
+++
@@ -1,5 +0,0 @@
-#include <config.h>
-#include <gtest/gtest.h>
-#include <version.hpp>
-
-TEST(VersionTest, GetVerPatch) { ASSERT_TRUE(get_ver_patch() > 0); }
